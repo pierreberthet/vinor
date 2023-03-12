ede4112b517d2608a04359aba84d57ae4ff27e73
pierreberthet <berthetp@gmail.com>
2023-03-12 18:14:24 +0100

    feat: added plots of country and quantity, and a dataframe for storing results from matching TF and VINMO

--- localdata.py
+++ localdata.py
@@ -21,6 +21,7 @@ import os
 
 from unidecode import unidecode
 
+from matplotlib import pyplot as plt
 import seaborn as sns
 
 
@@ -41,11 +42,40 @@ tf = pd.read_csv('dutyfree_df_14_01_2023.csv', index_col=0)
 
 
 #%%
+def despine(ax):
+    "Remove the top and right spines in pyplot figures."
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    return None
 
 
-sns.countplot(data=vinmo, x='country')
+#%%
 
 
+# VINMO
+f, ax = plt.subplots(figsize=(12, 8))
+sns.countplot(data=vinmo.sort_values(by='country'), x='country', ax=ax)
+ax.set_ylabel('# of entries')
+locs, labels=plt.xticks()
+plt.xticks(locs,labels, rotation=90)
+despine(ax)
+
+f, ax = plt.subplots(figsize=(12, 8))
+sns.swarmplot(data=vinmo, x='price', y='country', hue='type', ax=ax)
+despine(ax)
+
+
+# TF
+f, ax = plt.subplots(figsize=(12, 8))
+sns.countplot(data=tf.sort_values(by='country'), x='country', ax=ax)
+ax.set_ylabel('# of entries')
+locs, labels=plt.xticks()
+plt.xticks(locs,labels, rotation=90)
+despine(ax)
+
+f, ax = plt.subplots(figsize=(12, 8))
+sns.swarmplot(data=tf, x='price', y='country', hue='type', ax=ax)
+despine(ax)
 
 #%%   
 from thefuzz import process, fuzz
@@ -53,24 +83,49 @@ from thefuzz import process, fuzz
 # warning on slow SequenceMatcher. Install python-Levenshtein to remove this warning
 # restrict to wine type
 # cross check for volume and vintage
+# get those who failed the matching
 
 
 
 n_possibilities = 1
 cutoff = 90
 
+res = dict()
+
 start_time = time.time()
+
 for r, row in tf.iterrows():
     # add wine type in a query
     found, ratio = process.extractOne(row.wine, vinmo.wine.values, scorer=fuzz.token_sort_ratio)
+    res[r] = {'tf':row.wine, 'vinmo':vinmo.query("@found in wine"), 'tf_year': row.year, 'vinmo_year':vinmo.query("@found in wine").year.values,
+              'tf_price':row.price, 'vinmo_price':vinmo.query("@found in wine").price.values,
+              'difference':np.round(vinmo.query("@found in wine").price.values[0] - row.price, 2),
+              'percent':np.round(100*(vinmo.query("@found in wine").price.values[0] - row.price)/vinmo.query("@found in wine").price.values[0], 2),
+              'ratio':ratio}
     if  ratio > cutoff:
         # check volume and vintage
-    
+
         print(f'''{row.wine} matched with {found} at {ratio}\n \
               YEAR {row.year} --- {vinmo.query("@found in wine").year.values}\n\
               PRICE {row.price} --- {vinmo.query("@found in wine").price.values}\n\
               DIFF =  {np.round(vinmo.query("@found in wine").price.values[0] - row.price, 2)} NOK ---->  {np.round(100*(vinmo.query("@found in wine").price.values[0] - row.price)/vinmo.query("@found in wine").price.values[0], 2)}%   \n\n''')
         
+mdf = pd.DataFrame.from_dict(res, orient='index')
+        
 print(f"Took {time.time() - start_time} s")
 
+if False:
+    mdf.to_csv(os.path.join(folder, f"matched_TF_VINMO_{datetime.now().strftime('%d_%m_%Y')}.csv"))
+
+
 #%%
+
+
+
+
+
+#%%
+# TODO (plot)
+# plot dutyfree price and vinmo price, same range on axis, color coded by country? by grape?
+
+
